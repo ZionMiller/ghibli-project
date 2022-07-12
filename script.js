6457a7c99070e386f985862eafde334e3a308bfc
Zion Miller <djzionmiller@gmail.com>
2022-07-12 10:19:46 -0700

    adjust nav some commented out wip JS etc

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const filmsUrl = "https://ghibliapi.herokuapp.com/films"
+
+// const moveLeft = document.getElementsById("left-arrow");
+// const moveRight = document.getElementsById("right-arrow");
+
 const likeCount = document.getElementById("like-count")
+const filmCard = document.getElementById("movie-list")
+const filmDetails = document.getElementById("movie-details")
 let allMovieData
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -15,15 +21,33 @@ const getFilmData = async () => {
         })
 }
 
-// function renderFilms(films) {
-//     const filmArray = films
-//     filmArray.forEach((film)=>{
-        
-//     })
-// } 
+function renderFilms(films) {
+    const filmArray = films
+    // filmArray.forEach((films)=>{
+    //     let movieBox = document.createElement('li')
+    //     movieBox.src = films.image;
+    // })
+} 
+
+// document.getElementById("nav-dropdown").addEventListener('click', (e) => {
+//     console.log(e)
+// });
+
 
 document.getElementById("like-button").addEventListener("click", likeDisplay);
 
 function likeDisplay() {
     likeCount.textContent = parseInt(likeCount.textContent) + 1 + ` likes`
 }
+
+document.getElementById("left-arrow").addEventListener("click", (e) => {
+    console.log(e)
+    
+});
+
+document.getElementById("right-arrow").addEventListener('click', (e) => {
+    console.log(e)
+})
+
+
+renderFilms(); 
