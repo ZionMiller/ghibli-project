b91212bb2195808cdfbafb619275c6612b26b9c3
Zion Miller <djzionmiller@gmail.com>
2022-07-13 16:19:07 -0700

    appended drop down selector to reviews title, populated images, titles, descriptions from API, updated HTML to allow for dynamic rendering

--- script.js
+++ script.js
@@ -1,44 +1,113 @@
 const filmsUrl = "https://ghibliapi.herokuapp.com/films"
 
-// const moveLeft = document.getElementsById("left-arrow");
-// const moveRight = document.getElementsById("right-arrow");
 
-const likeCount = document.getElementById("like-count");
-const movieCard = document.getElementById("movie-card");
-const movieDetails = document.getElementById("movie-details");
-const movieTitle = document.getElementById("movie-title");
-const movieImg = document.getElementById("movie-img");
+const movieCollection = document.getElementById('movie-collection');
 
-// let allMovieData
+const selectMovie = document.getElementById("select-movie");
+// const cardBody = document.getElementById("card-body");
+const ghibRev = document.getElementById("nav-dropdown");
 
 // get film data from ghibli
 
 const init = () => {
 fetch(filmsUrl)
     .then(res => res.json())
-    .then(films => renderFilms(films))
+    .then(films => films.forEach(renderFilms))
 }
 
 function renderFilms(films) {
     const filmArray = films
-    console.log(filmArray)
+    // console.log(filmArray)
+
+    let movieCard = document.createElement('div');
+    movieCard.id = "card";
+
+    const img = document.createElement('img');
+    img.src = filmArray.image;
+    img.className = "movie-img";
+
+    const movieTitle = document.createElement('h2');
+    movieTitle.textContent = filmArray.title;
+
+    const movieDescription = document.createElement('p');
+    movieDescription.textContent = filmArray.description;
+
+    movieCard.append(img, movieTitle, movieDescription);
+    movieCollection.append(movieCard);
+
+            const nameDrop = document.createElement('option');
+            ghibRev.append(selectMovie)
+            selectMovie.append(nameDrop)
+            nameDrop.textContent = filmArray.title;
+            const div = document.createElement('div')
+    
+}
+
+// const dropDownHandle = films => {
+//     // films.forEach(film => {
+//         const nameDrop = document.createElement('option');
+//         ghibRev.append(selectMovie)
+//         selectMovie.append(nameDrop)
+//         nameDrop.textContent = films.title;
+//         const div = document.createElement('div')
+        
+//         renderFilms();
+        // const image = document.createElement('img')
+        // image.src = film.image
+        // div.append(image)
+        // cardBody.append(div)
+    // })
+
+    // selectMovie.addEventListener("change", chooseMovie)
+
+    //     function chooseMovie(e){
+    //          //const chosenMovie = e.target.value;
+
+    //          const filteredMovies = films.find((film)=>
+
+    //             (e.target.value.contains(film.name) ) 
+
+    //          )
+    //          console.log(filteredMovies) 
+    //     }
+// }
+
+
+// document.getElementById("nav-dropdown").addEventListener('click', (e) => {
+//     console.log(e)
+// });
+
+// // document.getElementById("like-button").addEventListener("click", likeDisplay);
+// // document.getElementById("comment-form-button").addEventListener("submit", handleComments);
+
+// function likeDisplay() {
+//     likeCount.textContent = parseInt(likeCount.textContent) + 1 + ` likes`
+// }
+
+// document.getElementById("left-arrow").addEventListener("click", (e) => {
+//     console.log(e)
+    
+// });
+
+// document.getElementById("right-arrow").addEventListener('click', (e) => {
+//     console.log(e)
+// })
+
+init();
+
+
+// commented out code
+
     // movieTitle.innerText = filmArray[0].title;
     // commented out because movie images contain titles
-    movieImg.src = filmArray[0].image;
-    movieDetails.textContent = filmArray[0].description;
-}
+    // movieImg.src = filmArray[0].image;
 
+// movieTitle.innerText = filmArray[0].title;
+// commented out because movie images contain titles
+// movieImg.src = filmArray[0].image;
 
-document.getElementById("nav-dropdown").addEventListener('click', (e) => {
-    console.log(e)
-});
+// movieDetails.textContent = filmArray[0].description;
 
-document.getElementById("like-button").addEventListener("click", likeDisplay);
-// document.getElementById("comment-form-button").addEventListener("submit", handleComments);
-
-function likeDisplay() {
-    likeCount.textContent = parseInt(likeCount.textContent) + 1 + ` likes`
-}
 // playing with submit form
 
 //  function handleComments() {
@@ -50,14 +119,3 @@ function likeDisplay() {
 //     })
 // }
 // handleComments();
-
-document.getElementById("left-arrow").addEventListener("click", (e) => {
-    console.log(e)
-    
-});
-
-document.getElementById("right-arrow").addEventListener('click', (e) => {
-    console.log(e)
-})
-// invoke init to get data
-init();
