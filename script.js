af12aa84ca01abe1d65c9ff69dc1bdd82acbce01
bandily <81126679+bandily@users.noreply.github.com>
2022-07-14 16:12:41 -0700

    test

--- script.js
+++ script.js
@@ -1,49 +1,149 @@
+//VOCABULARY
+//Modal = A modal (also called a modal window or lightbox) is a web page element that displays in front of and deactivates all other page content.
+
+//film = film describes the status of the entire program or an individual object. 
+
+//async function = is a function declared with the async keyword, and the await keyword is permitted within it. The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains.
+
 const filmsUrl = "https://ghibliapi.herokuapp.com/films"
-
-const movieCollection = document.getElementById('movie-collection');
-
-const selectMovie = document.getElementById("select-movie");
-// const cardBody = document.getElementById("card-body");
+const films = document.getElementById("films")
+const searchBar = document.getElementById('searchBar');
+const movieCollection = document.getElementById
+('movie-collection');
+//modal
+const synopsis = document.getElementsByClassName('synopsis');
+const overlay = document.getElementsByClassName('overlay');
+const openModalBtn = document.getElementsByClassName('openModalBtn');
+const closeModalBtn = document.getElementsByClassName('closeModalBtn');
+//dropdown
+const selectMovie = document.getElementById("drop-down");
 const ghibRev = document.getElementById("nav-dropdown");
 
 // get film data from ghibli
+let film;
 
-const init = () => {
-fetch(filmsUrl)
-    .then(res => res.json())
-    .then(films => films.forEach(renderFilms))
+async function fetchFilms() {
+    const response = await fetch("https://ghibliapi.herokuapp.com/films")
+    const data = await response.json();
+    film = data
+    renderFilms(film);
 }
+fetchFilms();
 
+//this functions displays the search results and renders the films to show on the page
+searchBar.addEventListener("keydown", (e) => {
+    const searchFilm = e.target.value.toLowerCase()
+    const filteredFilms = film.filter((film) => {
+        return (film.title.toLowerCase().includes(searchFilm))
+    });
+
+    renderFilms(filteredFilms);
+});
+
+//this functions displays the search results //this structure is the same structure from toytale. ntried to refactor but this wayt is quicker.
+//can possibly refactor, thsinis the way i learned to display searchresults and reset
 function renderFilms(films) {
     const filmArray = films
-    console.log(filmArray)
-
-    let movieCard = document.createElement('div');
-    movieCard.id = "card";
-
-    const img = document.createElement('img');
-    img.src = filmArray.image;
-    img.className = "movie-img";
-
-    const movieTitle = document.createElement('h2');
-    movieTitle.textContent = filmArray.title;
-
-    const movieDescription = document.createElement('p');
-    movieDescription.textContent = filmArray.description;
-
-    movieCard.append(img, movieTitle, movieDescription);
-    movieCollection.append(movieCard);
-
-            const nameDrop = document.createElement('option');
-            ghibRev.append(selectMovie)
-            selectMovie.append(nameDrop)
-            nameDrop.textContent = filmArray.title;
-            const div = document.createElement('div')
+        .map((films) => {
+            return ` 
+            <li class ="movie-collection">
+                    <h3>${films.title}</h3>
+                    <p>Year Released: ${films.release_date}</p>
+                    <h4.1>Directed by: ${films.director}</h4.1>
+                    <h4.2>Rating: ${films.rt_score}</h4.2>
+                    <img class="filmPoster" src="${films.image}" />
+                    <button class="openModalBtn" onclick="onClick(event)" value="${films.description}"> Synopsis </button>       
+                    <div id="containerForHeartIcon" >
+                        <i onclick="toggler(event)" id="heartIcon" class="fa-regular fa-heart"> </i>
+                    </div>                   
+            </li>`
+        })
+        .join(''); //using .join allows you to remove the commas from the array of films
+    movieCollection.innerHTML = filmArray
 }
+
+// this is the drop down copied from the other branch
+const dropDown = (e) => {
+    const target = e.target
+    const nameDrop = document.createElement('option')
+    ghibRev.append(selectMovie)
+    selectMovie.append(nameDrop)
+    nameDrop.textContent = filmArray.title
+    const div = document.createElement('div')
+}
+
+//these next functions will focus on the favorite button toggle can also add like number if want
+const toggler = (e) => {
+    const target = e.target;
+    console.log(target)
+    if (target.classList.contains("fa-regular")) {
+        target.classList.replace('fa-regular', 'fa-solid')
+
+    } else {
+        target.classList.replace('fa-solid', 'fa-regular')
+    }
+}
+
+// these functions are for the modal that popup when clicking the synopsis button
+const onClick = (e) => {
+    const target = e.target;
+    overlay[0].classList.toggle('hidden')
+    synopsis[0].innerHTML = target.value
+}
+
+closeModalBtn[0].addEventListener("click", (e) => {
+    console.log(e.target.value)
+    overlay[0].classList.toggle('hidden')
+})
+
+
+////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+// const init = () => {
+// fetch(filmsUrl)
+//     .then(res => res.json())
+//     .then(films => films.forEach(renderFilms))
+//     renderFilms(film);
+// }
+
+// function renderFilms(films) {
+//     const filmArray = films
+//     console.log(filmArray)
+
+//     let movieCard = document.createElement('div');
+//     movieCard.id = "card";
+
+//     const img = document.createElement('img');
+//     img.src = filmArray.image;
+//     img.className = "movie-img";
+
+//     const movieTitle = document.createElement('h2');
+//     movieTitle.textContent = filmArray.title;
+
+//     const movieDescription = document.createElement('p');
+//     movieDescription.textContent = filmArray.description;
+
+//     movieCard.append(img, movieTitle, movieDescription);
+//     movieCollection.append(movieCard);
+
+//             const nameDrop = document.createElement('option');
+//             ghibRev.append(selectMovie)
+//             selectMovie.append(nameDrop)
+//             nameDrop.textContent = filmArray.title;
+//             const div = document.createElement('div')
+// }
+
+
 // note: zion set const init above so we can pass 
 // for each to our render function as a whole easier
 // this invocation below is required to initialize the card
-init();
+// init();
 
 
 // break point commented out code from main
@@ -102,25 +202,25 @@ init();
 
 
 
-          // const image = document.createElement('img')
+//           const image = document.createElement('img')
 
-            // image.src = film.image
-            // div.append(image)
-            // cardBody.append(div)
+//             image.src = film.image
+//             div.append(image)
+//             cardBody.append(div)
 
 
-  // selectMovie.addEventListener("change", chooseMovie)
+//   selectMovie.addEventListener("change", chooseMovie)
 
-        //     function chooseMovie(e){
-        //          //const chosenMovie = e.target.value;
+//             function chooseMovie(e){
+//                  //const chosenMovie = e.target.value;
                  
-        //          const filteredMovies = films.find((film)=>
+//                  const filteredMovies = films.find((film)=>
                     
-        //             (e.target.value.contains(film.name) ) 
+//                     (e.target.value.contains(film.name) ) 
                     
-        //          )
-        //          console.log(filteredMovies) 
-        //     }
+//                  )
+//                  console.log(filteredMovies) 
+//             }
 
 
 // }
@@ -137,12 +237,12 @@ init();
 // }
 
 
-    // movieTitle.innerText = filmArray[0].title;
-    // commented out because movie images contain titles
-    // movieImg.src = filmArray[0].image;
-    // movieTitle.innerText = filmArray[0].title;
-    // movieImg.src = filmArray[0].image;
-    // movieDetails.textContent = filmArray[0].description;
+//     movieTitle.innerText = filmArray[0].title;
+//     commented out because movie images contain titles
+//     movieImg.src = filmArray[0].image;
+//     movieTitle.innerText = filmArray[0].title;
+//     movieImg.src = filmArray[0].image;
+//     movieDetails.textContent = filmArray[0].description;
 
 //  function handleComments() {
 //     let commentDisplay = document.getElementById("comment-display")
